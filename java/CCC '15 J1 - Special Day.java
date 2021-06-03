6980e104ca45195748c428fc8cad5356e5fd6479
Jensen <84945995+Jensen4@users.noreply.github.com>
2021-06-03 12:53:07 -0400

    Create CCC '15 J1 - Special Day.java

---
+++ java/CCC '15 J1 - Special Day.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        if (a <= 2 & b < 18) {
+            System.out.println("Before");
+        } else if (a >= 2 & b > 18) {
+            System.out.println("After");
+        } else {
+            System.out.println("Special");
+        }
+    }
+}
